8aa8e2c67cb45917c783f444410907033154e2f7
Divyanshu Bhoyar <divyanshubhoyar@yahoo.com>
2020-12-30 15:54:11 +0530

    bugs fixed

--- app.js
+++ app.js
@@ -119,11 +119,13 @@ client.on('message', message => {
     
         
         if (CMD_NAME == cmds.CMDs.announce) {
-            message.channel.send("Announcement made. Visit \#783028912220274698 ")
+            message.channel.send("Announcement made. Visit <\#783028912220274698> ")
             const text = args.join(" ");
-            client.channels.cache.get(`${DATA.aID}`).send(`📢  **${text}.**  -by: `)
+            client.channels.cache.get(`${DATA.aID}`).send(`📢  **${text}.**  -by:${message.author} `)
+	    client.channels.cache.get("752810283449450496").send('📢  Announcement made. Visit <\#783028912220274698> ')
+	    client.channels.cache.get("783075974996426782").send('📢  Announcement made. Visit <\#783028912220274698> ')
 			
-})
+
         return;}
 
 
